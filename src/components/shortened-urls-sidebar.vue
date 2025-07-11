c0cfa4d474935ee578faf8ff6852ff127daa0c13
Gabriel Guerra <gfranklings@gmail.com>
2025-07-11 17:46:45 -0400

    feat: Make the visit count update in real time

--- src/components/shortened-urls-sidebar.vue
+++ src/components/shortened-urls-sidebar.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
-import { useUrlsStore } from '@/stores/useUrlsStore';
-import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue'
+import { useUrlsStore } from '@/stores/useUrlsStore'
+import { storeToRefs } from 'pinia'
 
-import ShortenedUrlSideItem from './shortened-url-side-item.vue';
+import ShortenedUrlSideItem from './shortened-url-side-item.vue'
 
-const store = useUrlsStore();
-const { isSidebarOpen, urls, fetchingUrls } = storeToRefs(store);
+const store = useUrlsStore()
+const { isSidebarOpen, urls, fetchingUrls } = storeToRefs(store)
+
+type UpdateAction = 'visitCountUpdate';
+
+type VisitCountUpdate = {
+  action: UpdateAction
+  urlId: string
+  visitCount: number
+};
+
+const handleVisitCountUpdate = ({ urlId, visitCount }: VisitCountUpdate) => {
+  urls.value = urls.value.map(url => {
+    if (url.id === urlId) return {
+      ...url,
+      visitCount,
+    };
+    return url;
+  });
+}
+
+onMounted(() => {
+  const socket = new WebSocket(import.meta.env.VITE_WS_API_URL!)
+
+  socket.onmessage = (event: MessageEvent) => {
+    const parsed = JSON.parse(event.data as string) as VisitCountUpdate
+    // opened to add more actions in the future.
+    if (parsed.action === 'visitCountUpdate') {
+      handleVisitCountUpdate(parsed);
+    }
+  }
+});
 </script>
 
 <template>
@@ -15,12 +46,10 @@ const { isSidebarOpen, urls, fetchingUrls } = storeToRefs(store);
       class="fixed top-0 left-0 h-full bg-zinc-950 text-white w-80 p-4 border-r border-zinc-800"
     >
       <h2 class="text-xl font-semibold mb-4 mx-2">Shortened URLs</h2>
-      <ul class="list-none text-sm [&>li]:hover:bg-zinc-900 [&>li]:transition-colors [&>li]:duration-300 [&>li]:px-4 [&>li]:py-3 [&>li]:rounded-lg [&>li]:cursor-pointer">
-        <shortened-url-side-item
-          v-for="url in urls"
-          :key="url.id"
-          :url="url"
-        />
+      <ul
+        class="list-none text-sm [&>li]:hover:bg-zinc-900 [&>li]:transition-colors [&>li]:duration-300 [&>li]:px-4 [&>li]:py-3 [&>li]:rounded-lg [&>li]:cursor-pointer"
+      >
+        <shortened-url-side-item v-for="url in urls" :key="url.id" :url="url" />
       </ul>
     </aside>
   </transition>
